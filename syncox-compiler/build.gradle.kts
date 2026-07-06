209b5207a9f2eb0026310e8d913bbe74fba52d58
Samiu <samiuzhong@foxmail.com>
2026-07-06 11:52:28 +0800

    compiler 模块配置

--- syncox-compiler/build.gradle.kts
+++ syncox-compiler/build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlinJvm)
+    alias(libs.plugins.mavenPublish)
 }
 
 kotlin {
@@ -16,4 +17,39 @@ kotlin {
 
 dependencies {
     implementation(libs.ksp.symbol.processing)
+}
+
+mavenPublishing {
+    publishToMavenCentral()
+
+    signAllPublications()
+
+    coordinates("io.github.samiuzhong", "syncox-compiler", "0.1.0")
+
+    pom {
+        name = "Syncox"
+        description =
+            "The KSP annotation processor for Syncox engine, automatically generating routing tables for offline sync."
+        inceptionYear = "2026"
+        url = "https://github.com/SamiuZhong/Syncox"
+        licenses {
+            license {
+                name = "The Apache License, Version 2.0"
+                url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+                distribution = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+            }
+        }
+        developers {
+            developer {
+                id = "samiuzhong"
+                name = "Samiu Zhong"
+                url = "https://github.com/SamiuZhong"
+            }
+        }
+        scm {
+            url = "https://github.com/SamiuZhong/Syncox.git"
+            connection = "scm:git:git://github.com/SamiuZhong/Syncox.git"
+            developerConnection = "scm:git:git@github.com:SamiuZhong/Syncox.git"
+        }
+    }
 }
